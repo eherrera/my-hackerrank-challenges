1cd197aa9042287fca08649d7a19f5cd34569eff
eherrera <ernestoh2006@gmail.com>
2018-03-12 09:56:06 -0400

    FindDigits challenge

---
+++ Practice/FindDigits.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MyHackerrankChallenges.Practice
+{
+    public class FindDigits : IChallenge
+    {
+        public void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+            for (int a0 = 0; a0 < t; a0++)
+            {
+                int n = Convert.ToInt32(Console.ReadLine());
+                int result = findDigits(n);
+                Console.WriteLine(result);
+            }
+        }
+
+        static int findDigits(int n)
+        {
+            var digitsStr = n.ToString();
+            var result = 0;
+            foreach (var item in digitsStr)
+            {
+                var digit = int.Parse(item.ToString());
+                if (digit == 0)
+                {
+                    continue;
+                }
+                if (n % digit == 0)
+                {
+                    result++;
+                }
+            }
+            return result;
+        }
+
+    }
+}
