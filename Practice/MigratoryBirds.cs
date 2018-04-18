57cd873039bcfc8ff33fdb7cebbb40ad7b44e0f4
eherrera <ernestoh2006@gmail.com>
2018-04-18 18:04:30 -0400

    MigratoryBirds

---
+++ Practice/MigratoryBirds.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace MyHackerrankChallenges.Practice
+{
+    internal class MigratoryBirds : IChallenge
+    {
+        public void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = migratoryBirds(n, ar);
+            Console.WriteLine(result);
+        }
+
+        private int migratoryBirds(int i, int[] ar)
+        {
+            var g = ar.OrderBy(ave => ave).GroupBy(ave => ave).ToArray();
+            var max = g.Max(el => el.Count());
+            var val = g.FirstOrDefault(e => e.Count() == max);
+            return val?.Key ?? 0;
+        }
+    }
+}
