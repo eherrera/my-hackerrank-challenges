ada4a7d43a310a8d1f373a96aa21f3c48c5042b8
eherrera <ernestoh2006@gmail.com>
2018-05-07 09:20:40 -0400

    DrawingBook + BonAppetit

---
+++ Practice/BonAppetit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyHackerrankChallenges.Practice
+{
+    public class BonAppetit : IChallenge
+    {
+        public void Main(string[] args)
+        {
+            var firstLine = Array.ConvertAll(Console.ReadLine()?.Split(' '), int.Parse);
+            var items = Array.ConvertAll(Console.ReadLine()?.Split(' '), int.Parse);
+            var thirdLine = Console.ReadLine();
+            var result = GetBonAppetit(firstLine, items, int.Parse(thirdLine));
+            Console.WriteLine(result);
+        }
+
+        private static string GetBonAppetit(IReadOnlyList<int> firstLine, int[] itemsPrices, int bCharged)
+        {
+            var k = firstLine[1];
+            var sharedItemsTotalPrice = itemsPrices.Where((t, i) => i != k).Sum();
+            var bActual = sharedItemsTotalPrice / 2;
+            return bCharged == bActual ? "Bon Appetit" : (bCharged - bActual).ToString();   
+        }
+    }
+}
---
+++ Practice/DrawingBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyHackerrankChallenges.Practice
+{
+    public class DrawingBook : IChallenge
+    {
+        public void Main(string[] args)
+        {
+            var n = Convert.ToInt32(Console.ReadLine());
+            var p = Convert.ToInt32(Console.ReadLine());
+            var result = PageTurns(n, p);
+            Console.WriteLine(result);
+        }
+
+        private static int PageTurns(int n, int p)
+        {
+            var startFromFront = n / 2 >= p;
+            if (startFromFront)
+            {
+                return p / 2;
+            }
+            return n % 2 != 0 ? (n - p) / 2 : (n - p + 1) / 2;
+        }
+    }
+}
